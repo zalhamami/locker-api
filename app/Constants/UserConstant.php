dd65a58cfa534c80feff008315b2ce14cb4ab0a7
Farizal Hamami <farizalhamami@gmail.com>
2022-08-13 07:20:10 +0700

    Wrap constant using class

--- app/Constants/UserConstant.php
+++ app/Constants/UserConstant.php
@@ -1,6 +1,10 @@
 <?php
 
-const USER_ADMIN = 'admin';
-const USER_GUEST = 'guest';
-const USER_LECTURER = 'lecturer';
-const USER_STUDENT = 'student';
+namespace App\Constants;
+
+class UserConstant {
+    const USER_ADMIN = 'admin';
+    const USER_GUEST = 'guest';
+    const USER_LECTURER = 'lecturer';
+    const USER_STUDENT = 'student';
+}
